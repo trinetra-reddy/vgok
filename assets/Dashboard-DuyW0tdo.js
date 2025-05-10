412b1c7d03dfc7c32235f420ee07cebee496dbd1
Thrinethra <treddy@rh.com>
2025-05-10 14:05:38 +0530

    Updates

---
+++ assets/Dashboard-DuyW0tdo.js
@@ -0,0 +1,21 @@
+import{c as s,j as e,A as c,O as t,C as d}from"./index-DXQT00w1.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m15 9-6 6",key:"1uzhvr"}],["path",{d:"m9 9 6 6",key:"z0biqf"}]],i=s("circle-x",o);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M16 16s-1.5-2-4-2-4 2-4 2",key:"epbg0q"}],["line",{x1:"9",x2:"9.01",y1:"9",y2:"9",key:"yxxnd0"}],["line",{x1:"15",x2:"15.01",y1:"9",y2:"9",key:"1p4y9e"}]],x=s("frown",n);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M3 12h.01",key:"nlz23k"}],["path",{d:"M3 18h.01",key:"1tta3j"}],["path",{d:"M3 6h.01",key:"1rqtza"}],["path",{d:"M8 12h13",key:"1za7za"}],["path",{d:"M8 18h13",key:"1lx6n3"}],["path",{d:"M8 6h13",key:"ik3vkj"}]],m=s("list",h);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]],b=s("loader-circle",p),j=()=>{const a=[{label:"Total Topic",value:1,icon:e.jsx(m,{size:20,className:"text-blue-500"}),border:"border-blue-100",text:"text-blue-500",arrow:"text-blue-500",iconBorder:"border border-blue-500"},{label:"Pending Topic",value:1,icon:e.jsx(b,{size:20,className:"text-orange-400"}),border:"border-orange-100",text:"text-orange-400",arrow:"text-orange-400",iconBorder:"border border-orange-400"},{label:"Approved Topic",value:0,icon:e.jsx(d,{size:20,className:"text-green-500"}),border:"border-green-100",text:"text-green-500",arrow:"text-green-500",iconBorder:"border border-green-500"},{label:"Rejected Topic",value:0,icon:e.jsx(i,{size:20,className:"text-red-400"}),border:"border-red-100",text:"text-red-400",arrow:"text-red-400",iconBorder:"border border-red-400"}];return e.jsxs("div",{className:"min-h-screen bg-[#f9f9fb] flex flex-col md:flex-row p-6 gap-6 w-full",children:[e.jsxs("main",{className:"flex-1 space-y-6",children:[e.jsx("div",{className:"grid grid-cols-1 sm:grid-cols-2 gap-4",children:a.map((r,l)=>e.jsxs("div",{className:`flex items-center justify-between p-4 bg-white rounded-lg shadow-sm border ${r.border}`,children:[e.jsxs("div",{className:"flex items-center gap-3",children:[e.jsx("div",{className:`p-2 rounded bg-opacity-10 ${r.text} ${r.iconBorder}`,children:r.icon}),e.jsxs("div",{children:[e.jsx("div",{className:"text-xl font-semibold",children:r.value}),e.jsx("div",{className:"text-sm text-gray-500",children:r.label})]})]}),e.jsx(c,{size:18,className:`${r.arrow} ${r.iconBorder} rounded-full p-1`})]},l))}),e.jsxs("div",{className:"",children:[e.jsx("div",{className:"py-4 font-bold text-gray-700 border-b",children:"Latest Approved Topics"}),e.jsx("div",{className:"overflow-x-auto bg-white rounded-xl shadow",children:e.jsxs("table",{className:"w-full text-sm text-left",children:[e.jsx("thead",{className:"bg-[#4269c2] text-white",children:e.jsxs("tr",{children:[e.jsx("th",{className:"px-6 py-3",children:"TITLE"}),e.jsx("th",{className:"px-6 py-3",children:"SUBCATEGORY"}),e.jsx("th",{className:"px-6 py-3",children:"UP - DOWN VOTE"}),e.jsx("th",{className:"px-6 py-3",children:"ACTION"})]})}),e.jsx("tbody",{children:e.jsx("tr",{className:"text-center",children:e.jsx("td",{colSpan:4,className:"py-8 text-gray-400",children:e.jsxs("div",{className:"flex flex-col items-center justify-center text-gray-400",children:[e.jsx(x,{size:32}),e.jsx("p",{className:"mt-2 text-sm",children:"No data found"})]})})})})]})})]})]}),e.jsx(t,{})]})};export{j as default};
---
+++ assets/trash-2-DB99gQFj.js
@@ -0,0 +1,21 @@
+import{c as e}from"./index-DXQT00w1.js";/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["rect",{width:"20",height:"14",x:"2",y:"3",rx:"2",key:"48i651"}],["line",{x1:"8",x2:"16",y1:"21",y2:"21",key:"1svkeh"}],["line",{x1:"12",x2:"12",y1:"17",y2:"21",key:"vw1qmm"}]],n=e("monitor",c);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],a=e("plus",t);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]],h=e("search",o);/**
+ * @license lucide-react v0.487.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=[["path",{d:"M3 6h18",key:"d0wm0j"}],["path",{d:"M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6",key:"4alrt4"}],["path",{d:"M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2",key:"v07s0e"}],["line",{x1:"10",x2:"10",y1:"11",y2:"17",key:"1uufr5"}],["line",{x1:"14",x2:"14",y1:"11",y2:"17",key:"xtxkd"}]],i=e("trash-2",y);export{n as M,a as P,h as S,i as T};
